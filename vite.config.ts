5bc6b6fa81bdc3553f0d805c05ee937878bd937b
khalylvisuals <khalilmtarfi06@gmail.com>
2026-06-13 15:38:53 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/be-cute-spa/',
+  base: './',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
